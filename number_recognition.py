06f42bfe5e94c81c196418cf73995a12bb2336b6
Clems06 <70856864+Clems06@users.noreply.github.com>
2025-02-05 16:48:10 +0100

    Update number_recognition.py

--- number_recognition.py
+++ number_recognition.py
@@ -1,185 +1,134 @@
-import random
-import math
 import numpy as np
-import jsonpickle
-import atexit
-import json
+import random
+import time
 import keyboard
+import jsonpickle
+
+start_time = time.time()
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-class Weight:
-    def __init__(self):
+def der_sigmoid(x):
+    ex = np.exp(-x)
+    return ex / ((1 + ex) ** 2)
 
-        #Initialise randomly
-        self.weight = random.uniform(-1, 1)
-
-        #Variables that will store the sum of the wished changes for each example and then do the average later
-        self.total_wished_changes = 0
-        self.number_of_wished_changes = 0
+def leaky_ReLu(x):
+    return x * 0.01 if x < 0 else x
 
 
-class Neuron:
-    def __init__(self, layer, number, num_output_weights=0):
-        #Classifying variables
-        self.num_output_weights = num_output_weights
-        self.layer = layer
-        self.number = number
-
-        #Value of the neuron before the sigmoid squishification function
-        self.z = 0
-        #Value of the neuron after the signmoid function
-        self.value = 0
-
-        #Initialise the weights comming from this neuron
-        self.output_weights = []
-        for i in range(self.num_output_weights):
-            self.output_weights.append(Weight())
-
-        #Value of the bias
-        self.bias_weight = 0
-
-        # Variables that will store the sum of the wished changes for the bias for each example and then do the average later
-        self.total_wished_bias_changes = 0
-        self.number_wished_bias_changes = 0
-
-        #Calculated value of the derivative of the neuron with respect to the cost, times the derivative of the sigmoid
-        self.delta = 0
+def der_leaky_ReLu(x):
+    return 0.01 if x < 0 else 1
 
 
 class Net:
-    def __init__(self, topology, learning_rate=0.5, bias=1):
-        #Number of neurons for each layer
+    def __init__(self, topology, learning_rate=0.1):
         self.topology = topology
-
-        #Value that will multiply the bias_weight value of each neuron, 1 by default
-        self.bias = bias
-
-        #Self.layers is where all the neurons are stored. This is just to initialise it
-        self.topology.append(0)
-        self.layers = []
-        for layer in range(len(self.topology) - 1):
-            #Create a layer, add it a number of neurons and append it to self.layers
-            layer_toAdd = []
-            for i in range(self.topology[layer]):
-                layer_toAdd.append(Neuron(layer, i, self.topology[layer + 1]))
-            self.layers.append(layer_toAdd)
-        self.topology.pop()
-
         self.learning_rate = learning_rate
+        plage_random = 0.001
+        self.weights = [np.random.rand(topology[i + 1], topology[i])*plage_random*2-plage_random for i in range(len(topology) - 1)]
+        self.biases = [None] + [np.random.rand(topology[i + 1])*plage_random*2-plage_random for i in range(len(topology) - 1)]
 
-    #Function that goes throught each of the neurons of a certain layer and calculates z, then its actual value
-    def modifLayer(self, layer):
-        for neuron in range(len(self.layers[layer])):
-            #Z= sum of all the previous layers values * the weights + the bias
-            self.layers[layer][neuron].z = sum(
-                [inputNeuron.value * inputNeuron.output_weights[neuron].weight for inputNeuron in
-                 self.layers[layer - 1]]) + self.bias * self.layers[layer][neuron].bias_weight
+        """self.activation = np.vectorize(leaky_ReLu)
+        self.der_activation = np.vectorize(der_leaky_ReLu)"""
+        self.activation = np.vectorize(sigmoid)
+        self.der_activation = np.vectorize(der_sigmoid)
 
-            #Apply the sigmoid function to z to get the actual value
-            self.layers[layer][neuron].value = self.sigmoid(self.layers[layer][neuron].z)
+        self.weight_derivative = [np.zeros((topology[i + 1], topology[i])) for i in range(len(topology) - 1)]
+        self.bias_derivative = [None] + [np.zeros(topology[i + 1]) for i in range(len(topology) - 1)]
 
-    #Function that initialises the first layer according to the inputs, then calculates each layer according to its previous one
-    def calculateResult(self, inputs):
-        #Input layer
-        for i in range(len(inputs)):
-            self.layers[0][i].value = inputs[i]
+    def get_output(self, input: np.ndarray):
+        if len(input) != self.topology[0]:
+            raise ValueError("Expected input size {0} but got {1}".format(self.topology[0], len(input)))
 
-        #Calculate values of each layer
-        for layer in range(1, len(self.topology)):
-            self.modifLayer(layer)
+        values = input
+        for i in range(len(self.weights)):
+            values = self.activation(np.matmul(self.weights[i], values) + self.biases[i + 1])
 
-        #return the values of the output layer
-        return [neuron.value for neuron in self.layers[-1]]
+        return values
 
-    #Sigmoid function. To avoid OverflowError, when x is too high or too low it makes automatically 1 or 0
-    def sigmoid(self, x):
-        if x < -100:
-            return 0
-        if x > 100:
-            return 1
-        return 1 / (1 + np.exp(-x))
+    def with_backpropagation(self, input: np.ndarray, expected_output: np.ndarray):
+        if len(input) != self.topology[0]:
+            raise ValueError("Expected input size {0} but got {1}".format(self.topology[0], len(input)))
+        if len(expected_output) != self.topology[-1]:
+            raise ValueError("Expected output size {0} but got {1}".format(self.topology[1], len(expected_output)))
 
-    # math.exp(-z) / ((1 + math.exp(-z)) ** 2) = (1 / (1 + math.exp(-z))) * (1 - 1 / (1 + math.exp(-z))) = sigmoid * (1 - sigmoid) = value * (1 - value)
-    def DerivativeSigmoid(self, value):
-        return value * (1 - value)
+        layer_values = [input]
+        z_values = [0]
+        values = input
+        for i in range(len(self.weights)):
+            z = np.clip(np.matmul(self.weights[i], values) + self.biases[i + 1], -300, 300)
+            values = self.activation(z)
+            z_values.append(z)
+            layer_values.append(values)
 
-    #Funtion that takes inputs and expected outputs and calculates the wished changes for each bias and weight
-    def trainOnOneExample(self, inputs, expected_outputs):
+        values_vs_cost = [None] * (len(self.topology) - 1) + [2 * (layer_values[-1] - expected_output)]
+        for i in range(len(self.topology) - 1, 0, -1):
+            der_z = self.der_activation(z_values[i])
 
-        outputs_got = self.calculateResult(inputs)
+            self.bias_derivative[i] += der_z * values_vs_cost[i]
+            values_vs_cost[i-1] = np.array([np.sum(self.weights[i-1][:, j]* der_z *values_vs_cost[i]) for j in range(self.topology[i-1])])
+            self.weight_derivative[i - 1] += np.array([[layer_values[i - 1][k] * der_z[j] * values_vs_cost[i][j] for k in range(self.topology[i - 1])] for j in range(self.topology[i])])
 
-        errors = [(expected_outputs[i] - outputs_got[i]) ** 2 for i in range(len(expected_outputs))]
+    def reset_derivatives(self):
+        self.weight_derivative = [np.zeros((self.topology[i + 1], self.topology[i])) for i in
+                                  range(len(self.topology) - 1)]
+        self.bias_derivative = [None] + [np.zeros(self.topology[i + 1]) for i in range(len(self.topology) - 1)]
 
-        #A: Derivative of a neuron value: sum of ( (all the weights its got) x (derivative of the signmoid function) x (wished output of the neuron the weight leads to) ).
-        #   As the Derivative of a neuron value is always multilied by the derivative of the sigmoid, I call this product delta and use that
-        #B: Derivative of a weight: (value of the neuron it comes from) x (the derivative of the sigmoid function) x (the wished output of the neuron the weight leads to)
-        #C: Derivative of the bias: (derivative of the sigmoid function) x (the wished change of that neuron)
-        for idx in range(1,len(self.layers)+1):
-            layer_idx = -idx
-            for neuron_idx in range(len(self.layers[layer_idx])):
-                neuron = self.layers[layer_idx][neuron_idx]
-                #A
-                if layer_idx != -1:
-                    neuron_derVsCost = sum([neuron.output_weights[i].weight * self.layers[layer_idx+1][i].delta for i in range(self.topology[layer_idx+1])])
-                else:
-                    neuron_derVsCost = 2 * (expected_outputs[neuron_idx] - neuron.value)
+    def train(self, training_data, epochs=500, batch_size=20):
+        for epoch in range(epochs):
+            print("epoch=", epoch)
+            for _ in range(batch_size):
+                input, output = training_data[random.randint(0, len(training_data) - 1)]
+                self.with_backpropagation(np.array(input), np.array(output))
 
-                neuron.delta = neuron_derVsCost * self.DerivativeSigmoid(neuron.value)
-                #C
-                neuron.total_wished_bias_changes += neuron.delta
-                neuron.number_wished_bias_changes += 1
+            for a in range(len(self.topology)):
+                if a != len(self.topology) - 1:
+                    self.weights[a] -= self.weight_derivative[a] / batch_size * self.learning_rate
+                if a != 0:
+                    self.biases[a] -= self.bias_derivative[a] / batch_size * self.learning_rate
 
-                #Go throught each weight
-                for weight in range(len(neuron.output_weights)):
-                    this_weight = neuron.output_weights[weight]
-                    #B
-                    this_weight.total_wished_changes += neuron.value * self.layers[layer_idx+1][weight].delta
-                    this_weight.number_of_wished_changes += 1
+            self.reset_derivatives()
 
+    def train2(self, training_data, test_data, epochs=500, batch_size=20):
+        #keyboard.on_press_key("g", self.save_data)
 
-    def CorrectFromWished(self):
-        for layer in self.layers[1:]:
-            for neuron in layer:
-                neuron.bias_weight += (neuron.total_wished_bias_changes / neuron.number_wished_bias_changes) * self.learning_rate
-                neuron.total_wished_bias_changes = 0
-                neuron.number_wished_bias_changes = 0
+        for epoch in range(epochs):
+            print("epoch=",epoch)
+            permutation = np.random.permutation(training_data)
 
-                for weight in neuron.output_weights:
-                    weight.weight += (weight.total_wished_changes / weight.number_of_wished_changes) * self.learning_rate
-                    weight.total_wished_changes = 0
-                    weight.number_of_wished_changes = 0
+            for i in range(len(training_data)):
+                print("|", end="")
 
+                input, output = permutation[i]
 
+                self.with_backpropagation(np.array(input), np.array(output))
 
-    #Function that takes the training data and test data, trains the NN, modifies the values and test to see the improvement
-    def Training(self, training_data, test_data):
-        keyboard.on_press_key("g", self.save_data)
-        #Divide in mini batches for stochastic gradeint descend
-        random.shuffle(training_data)
-        batches = np.array_split(training_data, round(len(training_data)**0.5))
+                if (i!=0 and i%batch_size == 0) or i == len(training_data)-1:
+                    print()
+                    for a in range(len(self.topology)):
+                        if a != len(self.topology) - 1:
+                            self.weights[a] -= self.weight_derivative[a] / batch_size * self.learning_rate
+                        if a != 0:
+                            self.biases[a] -= self.bias_derivative[a] / batch_size * self.learning_rate
 
-        #lr = np.arange(10)
+                    self.reset_derivatives()
 
-        for epoch in range(1000):
-            for batch in batches:
-                for sample in batch:
-                    print("|",end="")
-
-                    #Trains an example
-                    expectedoutput = sample[0]
-                    self.trainOnOneExample(sample[1], expectedoutput)
-
-                self.CorrectFromWished()
-                print()
-
-            #Randomly select a test sample and print its result
             test_sample = test_data[random.randint(0, len(test_data) - 1)]
-            expectedoutput_test = test_sample[0]
-            actualoutput = self.calculateResult(test_sample[1])
-            error = sum([(expectedoutput_test[i] - actualoutput[i])**2 for i in range(len(expectedoutput_test))])
-            print("Epoch {0}: Error {1}      Expected {2} and Got {3}".format(epoch,error,expectedoutput_test,actualoutput))
+            expectedoutput_test = test_sample[1]
+            actualoutput = self.get_output(test_sample[0])
 
-    def save_data(self,evt):
+            error = sum([(expectedoutput_test[i] - actualoutput[i]) ** 2 for i in range(len(expectedoutput_test))])
+            print()
+            print("Epoch {0}: Error {1}      Expected {2} and Got {3}".format(epoch, error, expectedoutput_test,
+                                                                              actualoutput))
+
+            self.save_data()
+
+        self.save_data()
+
+    def save_data(self,evt=None):
         print("Initialising saving...")
         json_net = jsonpickle.encode(self)
         open('./save_net.json', 'w').close()
@@ -188,35 +137,52 @@ class Net:
         print('Net succesfully saved')
 
 
-
+nums = [0]*10
 def format_data(old_version):
     lr = np.arange(10)
 
     desired_number=old_version[0]
+    nums[int(desired_number)] += 1
     inputs=old_version[1:].tolist()
 
-    transdformed_wished_output = (lr == desired_number).astype(np.int).tolist()
+    return [inputs, (lr == desired_number).astype(np.int).tolist()]
 
-    return [transdformed_wished_output,inputs]
-
-def main():
+if __name__ == "__main__":
     image_size = 28
     no_of_different_labels = 10
     image_pixels = image_size * image_size
 
     data_path = ""
-    raw_train_data = np.loadtxt(data_path + "mnist_train.csv", delimiter="," , max_rows=500)
+    raw_train_data = np.loadtxt(data_path + "../mnist_train.csv", delimiter="," , max_rows=5000)
     print("Import train data complete")
     train_data = [format_data(i) for i in raw_train_data]
 
-    raw_test_data = np.loadtxt(data_path + "mnist_test.csv", delimiter=",", max_rows=50)
+    print(nums)
+
+    raw_test_data = np.loadtxt(data_path + "../mnist_test.csv", delimiter=",", max_rows=50)
     print("Import test data complete")
     test_data = [format_data(i) for i in raw_test_data]
 
-    net = Net([image_pixels, 100, 10])
-    net.Training(train_data,test_data)
+    net = Net([image_pixels, 300, 100, 10])
+    net.train2(train_data, test_data, batch_size=30)
 
-#Avoid creating new net if it is imported
-if __file__.split("/")[-1] == "number_recognition.py":
-    print("Starting new net...")
-    main()
+
+    """test = Net([3, 2, 2])
+    
+    examples = [[[0, 0, 1], [0, 1]], [[1, 0, 1], [1, 1]], [[0, 1, 1], [1, 1]], [[0, 0, 0], [0, 0]], [[1, 0, 0], [1, 0]], [[1, 1, 0], [0, 0]]]
+    
+    
+    
+    test.train(examples, 2000)
+
+    #test.train(examples)
+    
+    print("After")
+    print(test.get_output(np.array([1, 1, 1])))
+    print(test.get_output(np.array([1, 1, 0])))
+    print(test.get_output(np.array([0, 1, 0])))
+    print(test.get_output(np.array([0, 1, 1])))
+    print(test.get_output(np.array([1, 0, 0])))"""
+
+
+    print("Took {} seconds".format(time.time()-start_time))
